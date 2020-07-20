9d94ed037afca0e260aa1658022dae17d3fee041
Lucas Hans <lucas_hans@hotmail.com>
2020-07-20 09:36:37 -0300

    User Insert with post

--- src/main/java/com/lucashans/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucashans/workshopmongo/resources/UserResource.java
@@ -5,11 +5,11 @@ import com.lucashans.workshopmongo.dto.UserDTO;
 import com.lucashans.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,4 +32,13 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/main/java/com/lucashans/workshopmongo/services/UserService.java
+++ src/main/java/com/lucashans/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucashans.workshopmongo.services;
 
 import com.lucashans.workshopmongo.domain.User;
+import com.lucashans.workshopmongo.dto.UserDTO;
 import com.lucashans.workshopmongo.repositories.UserRepository;
 import com.lucashans.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
     }
 }
