220cf478d6b447a44d85c70f6b7d588ccf12ecfc
Lucas Hans <lucas_hans@hotmail.com>
2020-07-20 10:39:33 -0300

    Post and PostRepository

---
+++ src/main/java/com/lucashans/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.lucashans.workshopmongo.repositories;
+
+import com.lucashans.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
